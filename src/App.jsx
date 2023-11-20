09d2c96f4a6f9acace17c35e791a2430f12753bd
Ossan27 <rubossan@upv.edu.es>
2023-11-20 12:33:59 +0000

    changed Affiliation Test

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Rubén Ossorio",
+  title: "PhD Engineer",
+  email: "rubenossorio93@gmail.com",
   gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "ruben-ossorio",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
